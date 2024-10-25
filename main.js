efb5a21894b63107fcd62dd445ef86963cd1a552
Basmala-hub <basmala4198615@gmail.com>
2024-10-25 15:33:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// function check(num){
+//     if(num<=0)
+//         return true
+//     else
+//     return false
+// }
+// console.log(check(3));
+
+// ====================1================
+// function avar (num1,num2,num3,num4,num5,num6,num7,num8,num9,num10) {
+//     var avarage=(num1+num2+num3+num4+num5+num6+num7+num8+num9+num10)/10;
+//     return avarage;
+// }
+// console.log(avar (1,2,3,4,5,7,8,9,10,11));
+// ======================اعمل فانكشن تأخذ اكتر من عشر اسامي داخلarray ثم تعرض اول عنصر فقط
+// function array(arr) {
+//     return arr[0];
+// }
+// var arr=['ba','m'];
+// console.log(array(arr));
+var text="basmala";
+document.getElementById("dom").innerHTML=`is the first: ${text}`;
